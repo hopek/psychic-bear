8300b8d08b982b0bf6d86238915e526bdf7ed73d
jgugala <jgugala.pgs-soft.com>
2013-06-24 11:08:29 +0200

    New class added

---
+++ src/com/pgs/hopek/androidtesstgit/MainActivity.java
@@ -0,0 +1,45 @@
+package com.pgs.hopek.androidtesstgit;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private TextView mTextView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		((Button) findViewById(R.id.button1)).setOnClickListener(this);
+		mTextView = (TextView) findViewById(R.id.textView1);
+				
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.button1) {
+			mTextView.setText(new BigInteger(130, new SecureRandom()).toString(32));
+		}
+	}
+	
+	private void tmpMethod() {
+		
+	}
+
+}
