ad4969509fff9f3a212c102823181087cb6c1ba9
jgugala <jgugala.pgs-soft.com>
2013-06-25 09:39:44 +0200

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "- layout set to NewActivity - MainActivity button onClick action for start new activity"""""""
    This reverts commit b4599fc836b97bb7a027eb0f3c81d29973f2176f.

--- src/com/pgs/hopek/androidtesstgit/NewActivity.java
+++ src/com/pgs/hopek/androidtesstgit/NewActivity.java
@@ -8,7 +8,6 @@ public class NewActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_new);
 	}
 
 }
